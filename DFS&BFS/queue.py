d3163e5750ef7e64426310c0c1b26b2bf5de2eeb
minseok0616 <minseok6815@naver.com>
2021-08-08 03:08:27 +0900

    example

---
+++ DFS&BFS/queue.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+#큐(Queue)구현을 위해 deque라이브러리를 사용한다.
+
+queue = deque()
+
+queue.append(5)
+queue.append(2)
+queue.append(3)
+queue.append(7)
+queue.popleft()
+queue.append(1)
+queue.append(4)
+queue.popleft()
+
+print(queue)
+queue.reverse()
+print(queue)
+
+
