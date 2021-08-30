fe2505b74669a23229472a1fe26835163e5c29bb
minseok0616 <minseok6815@naver.com>
2021-08-30 22:38:12 +0900

    Create 1890.py

---
+++ dinamic programming/1890.py
@@ -0,0 +1,19 @@
+import sys
+n = int(sys.stdin.readline())
+maps = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
+table = [[0 for _ in range(n)] for _ in range(n)]
+
+table[0][0] = 1
+for y in range(n):
+    for x in range(n):
+
+        if table[y][x] != 0 and maps[y][x] != 0:
+
+            if y + maps[y][x] < n:
+                table[y + maps[y][x]][x] += table[y][x]
+           
+            if x + maps[y][x] < n:
+                table[y][x+ maps[y][x]] += table[y][x]
+
+print(table[-1][-1])
